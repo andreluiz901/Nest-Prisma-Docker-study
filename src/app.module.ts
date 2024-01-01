692b7e243aa7530114633205aba167a0f94747be
André Luiz <silva.ala82@gmail.com>
2024-01-01 17:32:44 -0300

    chore: adjust imports modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
-import { ProductsModule } from './products/products.module';
-import { CalculatorModule } from '@/src/calculator/calculator.module';
+import { ProductsModule } from './modules/products/products.module';
+import { CalculatorModule } from '@/src/modules/calculator/calculator.module';
 import { ConfigModule } from '@nestjs/config';
 import { CustomersModule } from './modules/customers/customers.module';
-import * as joi from 'joi'
+import { SecurityModule } from './modules/security/security.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, CalculatorModule, ConfigModule.forRoot({ validationSchema: joi.object({ TEST: joi.string().valid('development', 'production', 'test').required() }) }), CustomersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    UsersModule,
+    ProductsModule,
+    CalculatorModule,
+    CustomersModule,
+    SecurityModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
