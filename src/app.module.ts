e912c2a0f2a880a7f12c80c15e45a6e73aaac727
André Luiz <silva.ala82@gmail.com>
2024-01-06 18:43:56 -0300

    feat: implements auth guard to uthentication jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
-import { CalculatorModule } from '@/src/modules/calculator/calculator.module';
 import { ConfigModule } from '@nestjs/config';
 import { CustomersModule } from './modules/customers/customers.module';
 import { SecurityModule } from './modules/security/security.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { CalculatorModule } from './modules/calculator/calculator.module';
 
 @Module({
   imports: [
